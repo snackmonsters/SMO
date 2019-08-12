4e0a7f159c1ca2e97d02f4d196560778b8b3e0ff
박길성 <gilseong223@gmail.com>
2019-08-12 10:17:59 +0900

    Remove: remove test

--- test/src/main/java/com/ten/smoproject/Repository/PersonMongoDBRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ten.smoproject.Repository;
-
-import com.ten.smoproject.DTO.users;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PersonMongoDBRepository extends MongoRepository<users, String> {
-    public users findByName(String name);
-}
--- test/src/main/java/com/ten/smoproject/Repository/PersonMongoDBRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ten.smoproject.Repository;
-
-import com.ten.smoproject.DTO.users;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public class PersonMongoDBRepositoryImpl implements PersonMongoDBRepository {
-    public users findByName(String name);
-}
