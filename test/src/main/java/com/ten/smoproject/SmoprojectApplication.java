e6599ba9110c166bf7b7f886e277422449d3fe66
박길성 <gilseong223@gmail.com>
2019-08-12 10:23:25 +0900

    Add: restart test

---
+++ test/src/main/java/com/ten/smoproject/SmoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ten.smoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoprojectApplication.class, args);
+	}
+
+}
